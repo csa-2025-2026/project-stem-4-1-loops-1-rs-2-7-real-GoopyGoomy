015d9fdd8ffd833910f75c66283a7b709e5a6cf4
GoopyGoomy <earlgreytea0627@gmail.com>
2025-10-17 21:47:21 +0000

    a

--- Main.java
+++ Main.java
@@ -4,6 +4,35 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // Problem 1
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter numbers to add. Enter -1 to stop");
+    int num = 0;
+    int sum = 0;
+    while (num != -1)
+    {
+      num = sc.nextInt();
+      sum += num;
+    }
+    sum++;
+    System.out.println("The sun is: " + sum);
+
+    // Problem 2
+    System.out.println("How many inputs do you want?");
+    int input = sc.nextInt();
+    int count = 0;
+    int N = 0;
+    int max = 0;
+    System.out.println("Enter numbers");
+    while (count < input)
+    {
+      N = sc.nextInt();
+      if (N > max)
+      {
+        max = N;
+      }
+      count = count + 1;
+    }
+    System.out.println("The maximum value is " + max + ".");
   }
 }
