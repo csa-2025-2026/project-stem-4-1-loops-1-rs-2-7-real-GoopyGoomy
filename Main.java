13bc6ec5ef18d744b9ae506e2a87e0275629cf6d
GoopyGoomy <earlgreytea0627@gmail.com>
2025-10-23 21:12:41 +0000

    a

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main
   public static void main(String[] args)
   {
     // Problem 1
-    Scanner sc= new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     System.out.println("Enter numbers to add. Enter -1 to stop");
     int num = 0;
     int sum = 0;
@@ -34,5 +34,27 @@ public class Main
       count = count + 1;
     }
     System.out.println("The maximum value is " + max + ".");
+
+    //Problem 3
+    String word;
+
+    System.out.println("Input a word.");
+    sc.nextLine();
+    word = sc.nextLine();
+    int counter = 1;
+
+    int index = 0;
+    while (index < word.length())
+    {
+      String currentLetter = word.substring(index, index+1);
+      counter +=1;
+      index++;
+      if (counter == 3)
+      {
+        index++;
+        counter = 1;
+      }
+      System.out.println(currentLetter);
+    }
   }
 }
